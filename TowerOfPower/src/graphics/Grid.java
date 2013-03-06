dde5403bf153ad27ef974e602008b7ad21511579
41407 <jimi.welling@cs.helsinki.fi>
2013-03-07 01:00:12 +0200

    add Grid, Square, Location

---
+++ TowerOfPower/src/graphics/Grid.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphics;
+
+/**
+ *
+ * @author 41407
+ */
+public class Grid {
+    
+}
