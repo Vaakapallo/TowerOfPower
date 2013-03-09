562131483973f0d487c340ab77886763c5046988
41407 <jimi.welling@cs.helsinki.fi>
2013-03-09 23:47:23 +0200

    remove window, panels remove window, panels# Please enter the commit message for your changes. Lines starting

--- TowerOfPower/src/towerofpower/TowerOfPower.java
+++ TowerOfPower/src/towerofpower/TowerOfPower.java
@@ -4,14 +4,6 @@
  */
 package towerofpower;
 
-import graphics.GameWindow;
-import graphics.GodPanel;
-import graphics.draw.level.LevelPanel;
-import graphics.draw.level.UIPanel;
-import java.util.logging.Logger;
-import logic.Champion;
-import logic.Race;
-import logic.level.Level;
 import randomGenerators.WeaponNameGenerator;
 
 /**
@@ -28,26 +20,5 @@ public class TowerOfPower {
             System.out.println(WeaponNameGenerator.generateMeleeWeaponName());
             System.out.println(WeaponNameGenerator.generateRangedWeaponName());
         }
-
-        
-        Level a = new Level(16, 14);
-        a.getGrid().setCellSize(54);
-        
-        a.getGrid().getCellAt(3, 3).setVisibility(false);
-        a.getGrid().getCellAt(4, 3).setVisibility(false);
-        a.getGrid().getCellAt(3, 4).setVisibility(false);
-        a.getGrid().getCellAt(4, 4).setVisibility(false);
-        
-        Champion c = new Champion("Pekka", 30, 20, Race.Fighter);
-        
-        Champion d = new Champion("Makkonen", 1, 800, Race.Bowman);
-        a.getGrid().getCellAt(15,0).setContent(d);
-        
-        a.getGrid().getCellAt(10,4).setContent(c);
-        a.getGrid().getCellAt(3,8).setContent(d);
-        LevelPanel l = new LevelPanel(a);
-        UIPanel u = new UIPanel();
-        GodPanel g = new GodPanel(l, u);
-        GameWindow window = new GameWindow(g);
     }
 }
