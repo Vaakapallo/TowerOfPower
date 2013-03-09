989e8fee804590417bd8480d6baed34b9b6fc1fa
41407 <jimi.welling@cs.helsinki.fi>
2013-03-09 14:41:31 +0200

    add rudimentary image loader, to be refactored

--- TowerOfPower/src/graphics/draw/level/LevelDrawer.java
+++ TowerOfPower/src/graphics/draw/level/LevelDrawer.java
@@ -7,7 +7,12 @@ package graphics.draw.level;
 import graphics.grid.Grid;
 import graphics.grid.GridCell;
 import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
+import javax.imageio.ImageIO;
 import logic.level.Level;
 
 /**
@@ -21,11 +26,27 @@ public class LevelDrawer {
 
     void drawGrid(Grid grid, Graphics graphics) {
         ArrayList<GridCell> orderedCells = grid.getCells();
-        int x1, y1, x2, y2;
+        int dstx1, dsty1, dstx2, dsty2;
         for (GridCell c : orderedCells) {
-            x1 = c.getX();
-            y1 = c.getY();
-            graphics.fillRect(x1, y1, 10, 10);
+            dstx1 = c.getX();
+            dsty1 = c.getY();
+            dstx2 = dstx1 + 50;
+            dsty2 = dsty1 + 25;
+            Image img = getImage();
+            graphics.drawImage(img, dstx1, dsty1, dstx2, dsty2, 0, 0, 50, 25, null);
+
         }
+    }
+
+    private Image getImage() {
+        try {
+            BufferedImage img = null;
+            img = ImageIO.read(new File("src/assets/level/gridcell_50x25_transparency.png"));
+            return img;
+        } catch (IOException e) {
+            System.out.println("fack");
+            return null;
+        }
+
     }
 }
