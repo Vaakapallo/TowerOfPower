ed765999fa49e8261dc32a1a9ceb55e4d8d9628a
41407 <jimi.welling@cs.helsinki.fi>
2013-03-09 14:54:48 +0200

    add file structure for graphics, imageloader

---
+++ TowerOfPower/src/graphics/ImageLoader.java
@@ -0,0 +1,39 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package graphics;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+
+/**
+ * Loads an image from file.
+ * 
+ * @author 41407
+ */
+public class ImageLoader {
+
+    private Image i;
+
+    public ImageLoader(String path) {
+        this.i = loadImage(path);
+    }
+
+    public Image getImage() {
+        return i;
+    }
+
+    private Image loadImage(String path) {
+        try {
+            BufferedImage img = null;
+            img = ImageIO.read(new File(path));
+            return img;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+}
--- TowerOfPower/src/graphics/draw/level/LevelDrawer.java
+++ TowerOfPower/src/graphics/draw/level/LevelDrawer.java
@@ -4,6 +4,7 @@
  */
 package graphics.draw.level;
 
+import graphics.ImageLoader;
 import graphics.grid.Grid;
 import graphics.grid.GridCell;
 import java.awt.Graphics;
@@ -21,10 +22,13 @@ import logic.level.Level;
  */
 public class LevelDrawer {
 
+    String defaultPath = "resources/level/";
+    
     public void drawLevel(Level level, Graphics g) {
     }
 
     void drawGrid(Grid grid, Graphics graphics) {
+        String path = defaultPath+"grid/50x25/";
         ArrayList<GridCell> orderedCells = grid.getCells();
         int dstx1, dsty1, dstx2, dsty2;
         for (GridCell c : orderedCells) {
@@ -32,21 +36,14 @@ public class LevelDrawer {
             dsty1 = c.getY();
             dstx2 = dstx1 + 50;
             dsty2 = dsty1 + 25;
-            Image img = getImage();
-            graphics.drawImage(img, dstx1, dsty1, dstx2, dsty2, 0, 0, 50, 25, null);
+            graphics.drawImage(getImage(path+"00"), dstx1, dsty1, dstx2,
+                    dsty2, 0, 0, 50, 25, null);
 
         }
     }
 
-    private Image getImage() {
-        try {
-            BufferedImage img = null;
-            img = ImageIO.read(new File("src/assets/level/gridcell_50x25_transparency.png"));
-            return img;
-        } catch (IOException e) {
-            System.out.println("fack");
-            return null;
-        }
-
+    private Image getImage(String path) {
+        ImageLoader l = new ImageLoader(path+".png");
+        return l.getImage();
     }
 }
