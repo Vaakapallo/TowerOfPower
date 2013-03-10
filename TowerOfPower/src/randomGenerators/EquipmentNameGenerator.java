10fa3fe5b00920ee867a112b9515522c0df5899d
Vaakapallo <lassi.vapaakallio@cs.helsinki.fi>
2013-03-10 19:19:27 +0200

    Update EquipmentNameGenerator.java

--- TowerOfPower/src/randomGenerators/EquipmentNameGenerator.java
+++ TowerOfPower/src/randomGenerators/EquipmentNameGenerator.java
@@ -13,12 +13,14 @@ public class EquipmentNameGenerator {
     //THESE WILL ALL BE MOVED INTO TEXTFILES. Just trying out quick and dirty stuff now.
     /**
      * The Bottom Level is a nasty and dirty place.
+     * 
+     * Sock filled with garbage. Make the generation more adaptive and less wordy.
      */
-    private static String[] bottomLevelAdjectives = {"Shitty ", "Slimy ", "Glistening ", "Sticky ", "Brittle ", "Crappy ", "Sucky ", "Gross ", "Cracked ", "Slick ", "Rusty ", "Grimy ", "Dirty "};
+    private static String[] bottomLevelAdjectives = {"Shitty ", "Slimy ", "Glistening ", "Sticky ", "Brittle ", "Crappy ", "Sucky ", "Gross ", "Cracked ", "Slick ", "Rusty ", "Grimy ", "Dirty ", "Gnarly " , "Nasty ", "Bloody ", "Greasy "};
     private static String[] bottomLevelMeleeWeaponNouns = {"Shank", "Hammer", "Rock", "Brick", "Keg", "Stick", "Sickle", "Mallet", "Piece", "Pipe", "Spoon"};
-    private static String[] bottomLevelRangedWeaponHolders = {"Keg", "Jug", "Box", "Bag", "Pouch"};
-    private static String[] bottomLevelMeleeWeaponEndingNouns = {"Muck", "Garbage", "Mud", "Beer", "Decay", "Maggots", "Shame", "Shit", "Shame"};
-    private static String[] bottomLevelRangedWeaponAmmo = {"Garbage", "Bricks", "Maggots", "Roaches", "Plates", "Rocks"};
+    private static String[] bottomLevelRangedWeaponHolders = {"Keg", "Jug", "Box", "Bag", "Pouch", "Barrel"};
+    private static String[] bottomLevelMeleeWeaponEndingNouns = {"Muck", "Garbage", "Mud", "Beer", "Decay", "Maggots", "Shame", "Shit"};
+    private static String[] bottomLevelRangedWeaponAmmo = {"Garbage", "Bricks", "Maggots", "Roaches", "Plates", "Rocks", "Spiders" , "Goo", "Doorknobs", "Nails"};
     private static String[] meleeWeaponNameEndingNouns = {"axe", "hammer", "saber", "sickle", "mace", "sword"};
     private static String[] rangedWeaponNameEndingNouns = {"bow", "sling", "crossbow", "shotgun"};
 
